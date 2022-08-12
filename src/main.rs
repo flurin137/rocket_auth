925f5312974c80e39f5df8ec91d356e09dda401d
Flurin Feuerstein <flufeu@gmail.com>
2022-08-12 18:21:07 +0200

    added kinda database

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::sync::Mutex;
+
+use rocket::outcome::Outcome::*;
 use rocket::{
     http::{Cookie, CookieJar, Status},
     request::{FromRequest, Outcome},
-    Request,
+    Request, State,
 };
 
 #[macro_use]
@@ -10,48 +13,80 @@ extern crate rocket;
 const COOKIE_NAME: &str = "ASDF";
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    "Hello, world!".to_owned()
 }
 
 #[get("/secret")]
-fn secret(person: User) -> String {
+fn secret(database: &State<Mutex<Database>>, person: User) -> String {
+    if let Ok(database) = database.lock() {
+        return format!("Data {:?}", database.users);
+    }
+
     format!("Secret Data {}", person.name)
 }
 
 #[get("/login")]
-fn login(cookies: &CookieJar<'_>) -> &'static str {
-    cookies.add_private(Cookie::new(COOKIE_NAME, "asdf"));
+fn login(cookies: &CookieJar<'_>) -> String {
+    let user = "Fx";
+    cookies.add_private(Cookie::new(COOKIE_NAME, user));
+    format!("Logged in {}", user)
+}
 
-    "Logged in"
+#[get("/register")]
+fn register(cookies: &CookieJar<'_>, database: &State<Mutex<Database>>) -> String {
+    let user = "Fx";
+    cookies.add_private(Cookie::new(COOKIE_NAME, user));
+
+    if let Ok(mut database) = database.lock() {
+        database.users.push(User {
+            name: user.to_string(),
+        });
+    }
+
+    "Registered".to_owned()
 }
 
 #[get("/logout")]
-fn logout(cookies: &CookieJar<'_>) -> &'static str {
+fn logout(cookies: &CookieJar<'_>) -> String {
     cookies.remove_private(Cookie::named(COOKIE_NAME));
 
-    "Logged out"
+    "Logged out".to_owned()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, login, logout, secret])
+    rocket::build()
+        .manage(Mutex::new(Database { users: vec![] }))
+        .mount("/", routes![index, login, logout, register, secret])
 }
 
+struct Database {
+    users: Vec<User>,
+}
+
+#[derive(Debug, Clone)]
 struct User {
     name: String,
 }
 
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for User {
-    type Error = String;
+    type Error = ();
 
     async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        match req.cookies().get_private(COOKIE_NAME) {
-            Some(cookie) => Outcome::Success(User {
-                name: cookie.value().to_string(),
-            }),
-            None => Outcome::Failure((Status::Forbidden, "What are you doing here".to_string())),
-        }
+        let database = req.guard::<&State<Mutex<Database>>>().await;
+
+        if let Success(database) = database {
+            if let Ok(database) = database.lock() {
+                if let Some(cookie) = req.cookies().get_private(COOKIE_NAME) {
+                    let name = cookie.value().to_string();
+                    if let Some(user) = database.users.iter().find(|d| d.name == name) {
+                        return Success(user.clone());
+                    }
+                }
+            }
+        };
+        Failure((Status::BadRequest, ()))
     }
 }
